a92fa044b8a19f49487c9500284b2aa92b8862be
AhmadSyafarudin <72429818+AhmadSyafarudin@users.noreply.github.com>
2021-05-22 15:11:31 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,400 +1,263 @@
 import pygame
 import random
 
-pygame.font.init()
-
-# GLOBALS VARS
-s_width = 800
-s_height = 700
-play_width = 300  # meaning 300 // 10 = 30 width per block
-play_height = 600  # meaning 600 // 20 = 30 height per block
-block_size = 30
-
-top_left_x = (s_width - play_width) // 2
-top_left_y = s_height - play_height
-
-
-# SHAPE FORMATS
-
-S = [['.....',
-      '.....',
-      '..00.',
-      '.00..',
-      '.....'],
-     ['.....',
-      '..0..',
-      '..00.',
-      '...0.',
-      '.....']]
-
-Z = [['.....',
-      '.....',
-      '.00..',
-      '..00.',
-      '.....'],
-     ['.....',
-      '..0..',
-      '.00..',
-      '.0...',
-      '.....']]
-
-I = [['..0..',
-      '..0..',
-      '..0..',
-      '..0..',
-      '.....'],
-     ['.....',
-      '0000.',
-      '.....',
-      '.....',
-      '.....']]
-
-O = [['.....',
-      '.....',
-      '.00..',
-      '.00..',
-      '.....']]
-
-J = [['.....',
-      '.0...',
-      '.000.',
-      '.....',
-      '.....'],
-     ['.....',
-      '..00.',
-      '..0..',
-      '..0..',
-      '.....'],
-     ['.....',
-      '.....',
-      '.000.',
-      '...0.',
-      '.....'],
-     ['.....',
-      '..0..',
-      '..0..',
-      '.00..',
-      '.....']]
-
-L = [['.....',
-      '...0.',
-      '.000.',
-      '.....',
-      '.....'],
-     ['.....',
-      '..0..',
-      '..0..',
-      '..00.',
-      '.....'],
-     ['.....',
-      '.....',
-      '.000.',
-      '.0...',
-      '.....'],
-     ['.....',
-      '.00..',
-      '..0..',
-      '..0..',
-      '.....']]
-
-T = [['.....',
-      '..0..',
-      '.000.',
-      '.....',
-      '.....'],
-     ['.....',
-      '..0..',
-      '..00.',
-      '..0..',
-      '.....'],
-     ['.....',
-      '.....',
-      '.000.',
-      '..0..',
-      '.....'],
-     ['.....',
-      '..0..',
-      '.00..',
-      '..0..',
-      '.....']]
-
-shapes = [S, Z, I, O, J, L, T]
-shape_colors = [(0, 255, 0), (255, 0, 0), (0, 255, 255), (255, 255, 0), (255, 165, 0), (0, 0, 255), (128, 0, 128)]
-# index 0 - 6 represent shape
+#List Warna Blok untuk dikeluarkan secara Random
+warna = [
+    (0, 0, 0),
+    (120, 37, 179),
+    (100, 179, 179),
+    (80, 34, 22),
+    (80, 134, 22),
+    (180, 34, 22),
+    (180, 34, 122),
+]
 
 
-class Piece(object):  # *
-    def __init__(self, x, y, shape):
+class BlokTetris:
+    x = 0
+    y = 0
+
+    blok_tetris = [
+        [[1, 5, 9, 13], [4, 5, 6, 7]], #bentuk garis lurus
+        [[4, 5, 9, 10], [2, 6, 5, 9]], #bentuk Z
+        [[6, 7, 9, 10], [1, 5, 6, 10]], #bentuk S
+        [[1, 2, 5, 9], [0, 4, 5, 6], [1, 5, 9, 8], [4, 5, 6, 10]], #bentuk L
+        [[1, 2, 6, 10], [5, 6, 7, 9], [2, 6, 10, 11], [3, 5, 6, 7]], #bentuk J
+        [[1, 4, 5, 6], [1, 4, 5, 9], [4, 5, 6, 9], [1, 5, 6, 9]],  #bentuk T
+        [[1, 2, 5, 6]], #bentuk O
+    ]
+
+    def __init__(self, x, y):
         self.x = x
         self.y = y
-        self.shape = shape
-        self.color = shape_colors[shapes.index(shape)]
-        self.rotation = 0
+        self.bentuk_blok = random.randint(0, len(self.blok_tetris) - 1)
+        self.warna_blok = random.randint(1, len(warna) - 1)
+        self.ubah_bentuk = 0
+
+    def TampilanBlok(self):
+        return self.blok_tetris[self.bentuk_blok][self.ubah_bentuk]
+
+    def Putar(self):
+        self.ubah_bentuk = (self.ubah_bentuk + 1) % len(self.blok_tetris[self.bentuk_blok])
 
 
-def create_grid(locked_pos={}):  # *
-    grid = [[(0,0,0) for _ in range(10)] for _ in range(20)]
+class Papan :
+    x = 100
+    y = 60
+    zoom = 20
 
-    for i in range(len(grid)):
-        for j in range(len(grid[i])):
-            if (j, i) in locked_pos:
-                c = locked_pos[(j,i)]
-                grid[i][j] = c
-    return grid
+    def __init__(self, tinggi, lebar):
+        self.tinggi = tinggi
+        self.lebar = lebar
+        self.grid = []
+        self.state = "mulai"
+        for i in range(tinggi):
+            garis_grid = []
+            for j in range(lebar):
+                garis_grid.append(0)
+            self.grid.append(garis_grid)
 
+class Nilai(Papan) :
 
-def convert_shape_format(shape):
-    positions = []
-    format = shape.shape[shape.rotation % len(shape.shape)]
+  def __init__ (self, tinggi, lebar) :
+    super().__init__(tinggi, lebar) 
+    self.nilai = 0
+    self.jumlah_baris = 0 
+    
+  def BarisHilang(self):
+        baris = 0
+        for i in range(1, self.tinggi):
+            zeros = 0
+            for j in range(self.lebar):
+                if self.grid[i][j] == 0:
+                    zeros += 1
+            if zeros == 0:
+                baris += 1
+                self.jumlah_baris += 1
+                for k in range(i, 1, -1):
+                    for l in range(self.lebar):
+                        self.grid[k][l] = self.grid[k - 1][l]
+        
+        if self.jumlah_baris <= 3 :
+            if baris == 2 :
+                self.nilai += (baris ** 2) -1
+            if baris == 3 :
+                self.nilai += (baris ** 2) -2
+            if baris == 1 :
+                self.nilai += (baris ** 2) 
+        else :
+            self.nilai += 2 * self.jumlah_baris  
+            self.jumlah_baris = 0
 
-    for i, line in enumerate(format):
-        row = list(line)
-        for j, column in enumerate(row):
-            if column == '0':
-                positions.append((shape.x + j, shape.y + i))
+class Menu(Nilai) :
+    def __init__ (self, tinggi, lebar) :
+        super().__init__(tinggi, lebar)
 
-    for i, pos in enumerate(positions):
-        positions[i] = (pos[0] - 2, pos[1] - 4)
+    def MenuUtama (self) :
+        self.jenis_huruf = pygame.font.SysFont('Sans-Serrif', 23, True, False)
+        self.menu_1 = self.jenis_huruf.render("> Tekan Arah Bawah Untuk Permainan Baru", True, (255, 125, 0))
+        self.menu_2 = self.jenis_huruf.render("> Tekan Tombol 'C' Untuk Keluar", True, (255, 125, 0))
+        return self.menu_1, self.menu_2
 
-    return positions
-
-
-def valid_space(shape, grid):
-    accepted_pos = [[(j, i) for j in range(10) if grid[i][j] == (0,0,0)] for i in range(20)]
-    accepted_pos = [j for sub in accepted_pos for j in sub]
-
-    formatted = convert_shape_format(shape)
-
-    for pos in formatted:
-        if pos not in accepted_pos:
-            if pos[1] > -1:
-                return False
-    return True
-
-
-def check_lost(positions):
-    for pos in positions:
-        x, y = pos
-        if y < 1:
-            return True
-
-    return False
-
-
-def get_shape():
-    return Piece(5, 0, random.choice(shapes))
-
-
-def draw_text_middle(surface, text, size, color):
-    font = pygame.font.SysFont("comicsans", size, bold=True)
-    label = font.render(text, 1, color)
-
-    surface.blit(label, (top_left_x + play_width /2 - (label.get_width()/2), top_left_y + play_height/2 - label.get_height()/2))
-
-
-def draw_grid(surface, grid):
-    sx = top_left_x
-    sy = top_left_y
-
-    for i in range(len(grid)):
-        pygame.draw.line(surface, (128,128,128), (sx, sy + i*block_size), (sx+play_width, sy+ i*block_size))
-        for j in range(len(grid[i])):
-            pygame.draw.line(surface, (128, 128, 128), (sx + j*block_size, sy),(sx + j*block_size, sy + play_height))
-
-
-def clear_rows(grid, locked):
-
-    inc = 0
-    for i in range(len(grid)-1, -1, -1):
-        row = grid[i]
-        if (0,0,0) not in row:
-            inc += 1
-            ind = i
-            for j in range(len(row)):
-                try:
-                    del locked[(j,i)]
-                except:
-                    continue
-
-    if inc > 0:
-        for key in sorted(list(locked), key=lambda x: x[1])[::-1]:
-            x, y = key
-            if y < ind:
-                newKey = (x, y + inc)
-                locked[newKey] = locked.pop(key)
-
-    return inc
-
-
-def draw_next_shape(shape, surface):
-    font = pygame.font.SysFont('comicsans', 30)
-    label = font.render('Next Shape', 1, (255,255,255))
-
-    sx = top_left_x + play_width + 50
-    sy = top_left_y + play_height/2 - 100
-    format = shape.shape[shape.rotation % len(shape.shape)]
-
-    for i, line in enumerate(format):
-        row = list(line)
-        for j, column in enumerate(row):
-            if column == '0':
-                pygame.draw.rect(surface, shape.color, (sx + j*block_size, sy + i*block_size, block_size, block_size), 0)
-
-    surface.blit(label, (sx + 10, sy - 30))
-
-
-def update_score(nscore):
-    score = max_score()
-
-    with open('scores.txt', 'w') as f:
-        if int(score) > nscore:
-            f.write(str(score))
-        else:
-            f.write(str(nscore))
-
-
-def max_score():
-    with open('scores.txt', 'r') as f:
-        lines = f.readlines()
-        score = lines[0].strip()
-
-    return score
-
-
-def draw_window(surface, grid, score=0, last_score = 0):
-    surface.fill((0, 0, 0))
-
-    pygame.font.init()
-    font = pygame.font.SysFont('comicsans', 60)
-    label = font.render('Tetris', 1, (255, 255, 255))
-
-    surface.blit(label, (top_left_x + play_width / 2 - (label.get_width() / 2), 30))
-
-    # current score
-    font = pygame.font.SysFont('comicsans', 30)
-    label = font.render('Score: ' + str(score), 1, (255,255,255))
-
-    sx = top_left_x + play_width + 50
-    sy = top_left_y + play_height/2 - 100
-
-    surface.blit(label, (sx + 20, sy + 160))
-    # last score
-    label = font.render('High Score: ' + last_score, 1, (255,255,255))
-
-    sx = top_left_x - 200
-    sy = top_left_y + 200
-
-    surface.blit(label, (sx + 20, sy + 160))
-
-    for i in range(len(grid)):
-        for j in range(len(grid[i])):
-            pygame.draw.rect(surface, grid[i][j], (top_left_x + j*block_size, top_left_y + i*block_size, block_size, block_size), 0)
-
-    pygame.draw.rect(surface, (255, 0, 0), (top_left_x, top_left_y, play_width, play_height), 5)
-
-    draw_grid(surface, grid)
-    #pygame.display.update()
-
-
-def main(win):  # *
-    last_score = max_score()
-    locked_positions = {}
-    grid = create_grid(locked_positions)
-
-    change_piece = False
-    run = True
-    current_piece = get_shape()
-    next_piece = get_shape()
-    clock = pygame.time.Clock()
-    fall_time = 0
-    fall_speed = 0.27
-    level_time = 0
-    score = 0
-
-    while run:
-        grid = create_grid(locked_positions)
-        fall_time += clock.get_rawtime()
-        level_time += clock.get_rawtime()
-        clock.tick()
-
-        if level_time/1000 > 5:
-            level_time = 0
-            if level_time > 0.12:
-                level_time -= 0.005
-
-        if fall_time/1000 > fall_speed:
-            fall_time = 0
-            current_piece.y += 1
-            if not(valid_space(current_piece, grid)) and current_piece.y > 0:
-                current_piece.y -= 1
-                change_piece = True
-
+    def AmbilAksi (self) :
         for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                run = False
-                pygame.display.quit()
+            if event.type == pygame.KEYUP :
+                if event.key == pygame.K_DOWN :
+                    return True
+                if event.key == pygame.K_c :
+                    pygame.quit()
 
-            if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_LEFT:
-                    current_piece.x -= 1
-                    if not(valid_space(current_piece, grid)):
-                        current_piece.x += 1
-                if event.key == pygame.K_RIGHT:
-                    current_piece.x += 1
-                    if not(valid_space(current_piece, grid)):
-                        current_piece.x -= 1
-                if event.key == pygame.K_DOWN:
-                    current_piece.y += 1
-                    if not(valid_space(current_piece, grid)):
-                        current_piece.y -= 1
-                if event.key == pygame.K_UP:
-                    current_piece.rotation += 1
-                    if not(valid_space(current_piece, grid)):
-                        current_piece.rotation -= 1
+            if event.type == pygame.QUIT :
+                pygame.quit()
+                
+        return None
+        
 
-        shape_pos = convert_shape_format(current_piece)
+class Tetris(Menu):
+    level = 2
+    blok = None
 
-        for i in range(len(shape_pos)):
-            x, y = shape_pos[i]
-            if y > -1:
-                grid[y][x] = current_piece.color
+    def __init__(self, tinggi, lebar):
+       super().__init__(tinggi, lebar)
 
-        if change_piece:
-            for pos in shape_pos:
-                p = (pos[0], pos[1])
-                locked_positions[p] = current_piece.color
-            current_piece = next_piece
-            next_piece = get_shape()
-            change_piece = False
-            score += clear_rows(grid, locked_positions) * 10
+    def BuatBlok(self):
+        self.blok = BlokTetris(3, 0)
 
-        draw_window(win, grid, score, last_score)
-        draw_next_shape(next_piece, win)
-        pygame.display.update()
-
-        if check_lost(locked_positions):
-            draw_text_middle(win, "YOU LOST!", 80, (255,255,255))
-            pygame.display.update()
-            pygame.time.delay(1500)
-            run = False
-            update_score(score)
+    def CekLayar(self):
+        batas_atas_layar = False
+        for i in range(4):
+            for j in range(4):
+                if i * 4 + j in self.blok.TampilanBlok():
+                    if i + self.blok.y > self.tinggi - 1 or \
+                            j + self.blok.x > self.lebar - 1 or \
+                            j + self.blok.x < 0 or \
+                            self.grid[i + self.blok.y][j + self.blok.x] > 0:
+                        batas_atas_layar = True
+        return batas_atas_layar
 
 
-def main_menu(win):  # *
-    run = True
-    while run:
-        win.fill((0,0,0))
-        draw_text_middle(win, 'Press Any Key To Play', 60, (255,255,255))
-        pygame.display.update()
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                run = False
-            if event.type == pygame.KEYDOWN:
-                main(win)
+    def BlokJatuh(self):
+        self.blok.y += 1
+        if self.CekLayar():
+            self.blok.y -= 1
+            self.BlokBerhenti()
 
-    pygame.display.quit()
+    def BlokBerhenti(self):
+        for i in range(4):
+            for j in range(4):
+                if i * 4 + j in self.blok.TampilanBlok():
+                    self.grid[i + self.blok.y][j + self.blok.x] = self.blok.warna_blok
+        self.BarisHilang()
+        self.BuatBlok()
+        if self.CekLayar():
+            self.state = "berakhir"
+
+    def GerakSamping(self, dx):
+        blok_sebelum_pindah = self.blok.x
+        self.blok.x += dx
+        if self.CekLayar():
+            self.blok.x = blok_sebelum_pindah
+
+    def Putar(self):
+        blok_sebelum_diubah = self.blok.ubah_bentuk
+        self.blok.Putar()
+        if self.CekLayar():
+            self.blok.ubah_bentuk = blok_sebelum_diubah
 
 
-win = pygame.display.set_mode((s_width, s_height))
-pygame.display.set_caption('Tetris')
-main_menu(win)
+pygame.init()
+
+hitam = (0, 0, 0)
+putih = (255, 255, 255)
+abu = (128, 128, 128)
+
+ukuran_layar = (400, 500)
+layar = pygame.display.set_mode(ukuran_layar)
+
+pygame.display.set_caption("Tetris")
+
+
+clock = pygame.time.Clock()
+fps = 25
+tetris = Tetris(20, 10)
+menu_1, menu_2 = tetris.MenuUtama()
+layar.blit(menu_1, [20, 200 ])
+layar.blit(menu_2, [25, 265])
+
+while tetris.AmbilAksi() == None:
+    pygame.display.update()
+    clock.tick()
+
+berakhir = False
+counter = 0
+
+tekan_arah_bawah = False
+
+while not berakhir:
+    if tetris.blok is None:
+        tetris.BuatBlok()
+    counter += 1
+    if counter > 100000:
+        counter = 0
+
+    if counter % (fps // tetris.level // 2) == 0 or tekan_arah_bawah:
+        if tetris.state == "mulai":
+            tetris.BlokJatuh()
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            berakhir = True
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_UP:
+                tetris.Putar()
+            if event.key == pygame.K_DOWN:
+                tekan_arah_bawah = True
+            if event.key == pygame.K_LEFT:
+                tetris.GerakSamping(-1)
+            if event.key == pygame.K_RIGHT:
+                tetris.GerakSamping(1)
+            if event.key == pygame.K_ESCAPE:
+                tetris = Tetris(20, 10)
+            if event.key == pygame.K_c :
+                pygame.quit()
+
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_DOWN:
+                tekan_arah_bawah = False
+
+    layar.fill(putih)
+
+    for i in range(tetris.tinggi):
+        for j in range(tetris.lebar):
+            pygame.draw.rect(layar, abu, [tetris.x + tetris.zoom * j, tetris.y + tetris.zoom * i, tetris.zoom, tetris.zoom], 1)
+            if tetris.grid[i][j] > 0:
+                pygame.draw.rect(layar, warna[tetris.grid[i][j]],
+                                 [tetris.x + tetris.zoom * j + 1, tetris.y + tetris.zoom * i + 1, tetris.zoom - 2, tetris.zoom - 1])
+
+    if tetris.blok is not None:
+        for i in range(4):
+            for j in range(4):
+                p = i * 4 + j
+                if p in tetris.blok.TampilanBlok():
+                    pygame.draw.rect(layar, warna[tetris.blok.warna_blok],
+                                     [tetris.x + tetris.zoom * (j + tetris.blok.x) + 1,
+                                      tetris.y + tetris.zoom * (i + tetris.blok.y) + 1,
+                                      tetris.zoom - 2, tetris.zoom - 2])
+
+    jenis_huruf = pygame.font.SysFont('Sans-Serrif', 25, True, False)
+    jenis_huruf_2 = pygame.font.SysFont('Sans-Serrif', 30, True, False)
+    jenis_huruf_3 = pygame.font.SysFont('Sans-Serrif', 20, True, False)
+    score = jenis_huruf.render("Score: " + str(tetris.nilai), True, hitam)
+    permainan_berakhir = jenis_huruf_2.render("Permainan Berakhir", True, (232, 18, 18))
+    pilihan_1 = jenis_huruf_3.render("> Tekan 'ESC' untuk Permainan Baru", True, (255, 215, 0))
+    pilihan_2 = jenis_huruf_3.render("> Tekan 'C' untuk Keluar", True, (255, 215, 0))
+
+    layar.blit(score, [150, 0])
+    if tetris.state == "berakhir":
+        layar.blit(permainan_berakhir, [90, 20])
+        layar.blit(pilihan_1, [75, 460])
+        layar.blit(pilihan_2, [75, 475])
+        
+
+    pygame.display.flip()
+    clock.tick(fps)
